0c62a3f98df92fe6229b7fd492de63a5c9612e51
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-26 10:19:17 +0300

    Refactored the program to not use the redux store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,11 @@
-import store from '@/app/store'
 import theme from '@/theme'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
-import Provider from 'react-redux/es/components/Provider'
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<Provider store={store}>
-			<ChakraProvider theme={theme}>
-				<Component {...pageProps} />
-			</ChakraProvider>
-		</Provider>
+		<ChakraProvider theme={theme}>
+			<Component {...pageProps} />
+		</ChakraProvider>
 	)
 }
