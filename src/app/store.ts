cbd6143e93368214248ca977cbff0560c343ab5a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-26 10:34:33 +0300

    Finished setting up the redux store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,3 +1,15 @@
 import { usersApi } from '@/services/users'
 import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
+
+const store = configureStore({
+	reducer: {
+		[usersApi.reducerPath]: usersApi.reducer
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(usersApi.middleware)
+})
+
+setupListeners(store.dispatch)
+
+export default store
