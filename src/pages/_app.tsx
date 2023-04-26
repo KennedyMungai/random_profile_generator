2aa9fd27e44fdab3a37ac7521ee17e7e281a23a0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-26 10:35:25 +0300

    Set up the store for the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
+import store from '@/app/store'
 import theme from '@/theme'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<ChakraProvider theme={theme}>
-			<Component {...pageProps} />
-		</ChakraProvider>
+		<Provider store={store}>
+			<ChakraProvider theme={theme}>
+				<Component {...pageProps} />
+			</ChakraProvider>
+		</Provider>
 	)
 }
