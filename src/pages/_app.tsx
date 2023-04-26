98ef0ae12b72129a2b326d7f4f6148f770c318df
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-26 10:04:26 +0300

    Set up the Provider and the ChakraProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,14 @@
+import store from '@/app/store'
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import Provider from 'react-redux/es/components/Provider'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<Provider store={store}>
+			<ChakraProvider>
+				<Component {...pageProps} />)
+			</ChakraProvider>
+		</Provider>
+	)
 }
