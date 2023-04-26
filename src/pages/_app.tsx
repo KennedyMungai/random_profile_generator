ca349e179cae9e9641d9cc46c101604a432b3a63
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-26 10:05:07 +0300

    Got rid of an errant character

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<Provider store={store}>
 			<ChakraProvider>
-				<Component {...pageProps} />)
+				<Component {...pageProps} />
 			</ChakraProvider>
 		</Provider>
 	)
